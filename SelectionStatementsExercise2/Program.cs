7c88ac97b7addc317d16211122eddeecfe5c0146
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-15 21:53:02 -0400

    Exercise 2 Completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is a tough subject");
+                    break;
+
+                case "english":
+                    Console.WriteLine("English is an interesting subject");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History is a good subject");
+                    break;
+
+                default:
+                    Console.WriteLine($"Sounds fun I have never taken {subject} before.");
+                    break;  
+
+            }
         }
     }
 }
